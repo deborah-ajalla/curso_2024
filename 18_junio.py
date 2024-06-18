2309635c834f0bf2ff37d1f95fe70832e2be8df8
deborah-ajalla <deborahajalla@gmail.com>
2024-06-18 19:57:31 -0300

    agrrego contenido

--- 18_junio.py
+++ 18_junio.py
@@ -1,1 +1,48 @@
 #clase martes 18 junio
+#----------------------------
+# NÚMEROS
+print (15 + 8 )
+
+print (30.5 - 5)
+
+print (80*2)
+
+#----------------------------
+# STRINGS  -- STR --
+#"COMILLAS DOBLES"
+#'COMILLAS SIMPLES'
+
+print ("Hola, estamos en un curso de python")
+
+print ("Hola! quiero mostrar un string")
+
+print ("Un string \t con tabulacion")
+
+print ("un string pero con \n salto de linea")
+
+#----------------------------
+#VARIABLES
+""" las variables son como eqtiquetas
+    nos permiten hacer referencia a los datos
+    por cada dato que aparece en un programa,
+    python va a crear un objeto que lo contiene.
+    👉🏻 cada objeto va a contener:
+     1- un identificador unico ID
+     2- un tipo de dato (entero, decimal, string, etc)
+     3- un valor (el propio dato adentro.     
+"""
+
+dni = 32561456
+
+a = 2
+
+print(a)
+print(dni)
+
+#--------------------------------------------
+#definir una variable
+
+x = -40
+print("El valor de mi variable x es", x)
+
+
