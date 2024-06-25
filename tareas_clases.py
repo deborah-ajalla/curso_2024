23281c5b864f42728fa69a487010a43d9c8b8c1c
deborah-ajalla <deborahajalla@gmail.com>
2024-06-25 16:46:36 -0300

    resolucion

--- tareas_clases.py
+++ tareas_clases.py
@@ -1,5 +1,4 @@
-#DESTINADO A LAS TAREAS
-
+#TAREA:
 
 #CLASE 18/6: CREAR UN SCRIPT EN EL QUE PODAMOS CALCULAR LA NOTA FINAL EN BASE A 5 EXAMENES, ESTOS EXAMENES EQUIVALEN A UN PORCENTAJE DE LA NOTA FINAL.
 
@@ -11,3 +10,36 @@
 
 #A esto se lo suele llamar promedio ponderado: no todos los valores tienen el mismo "peso".
 #Por ejemplo, dado el ejercicio de arriba me conviene sacarme una mejor nota en el examen donde la nota vale casi el 50% de la nota final.
+
+#-------------------------------------------------------------------------------------------------------------------------------
+print ("---------------------------------------------------------------------")
+
+print ("El presente programa permite la carga de notas y obtiene su promedio")
+
+print ("---------------------------------------------------------------------")
+
+#Carga de Datos:
+nota1 = int (input("Ingrese la primera nota: "))
+nota2 = int (input ("Ingrese la segunda nota: "))
+nota3 = int (input ("Ingrese la tercera nota: "))
+nota4 = int (input ("Ingrese la cuarta nota: "))
+nota5 = int (input ("Ingrese la quinta nota: "))
+
+#Función:  PROMEDIO PONDERADO 
+# --> multiplicar cada nota por su respectivo porcentaje <--
+nota1 = nota1 * 0.2
+nota2 = nota2 * 0.1
+nota3 = nota3 * 0.1
+nota4 = nota4 * 0.1
+nota5 = nota5 * 0.5
+
+#--> se suman los resultados obtenidos <--
+nota_promedio = nota1 + nota2 + nota3 + nota4 + nota5
+
+# --> se imprime el resultado <--
+print ("---------------------------------------------------------------------")
+print (" --> El Promedio es:", nota_promedio)
+print ("---------------------------------------------------------------------")
+print("GRACIAS POR USAR NUESTRO PROGRAMA!!!")
+
+
