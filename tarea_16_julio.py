d93a033496fe31757ea8e7e6248d51ecff71cb38
deborah-ajalla <deborahajalla@gmail.com>
2024-07-23 15:56:26 -0300

    se cambia código

--- tarea_16_julio.py
+++ tarea_16_julio.py
@@ -1,8 +1,10 @@
 #CLASE 16 de Julio
-# analizar el código y explicar que hace cada linea,
-# luego reemplazarlo por otra condición
+
+# analizar el código y explicar que hace cada linea ✔
+# luego reemplazarlo por otra condición ✔
 #-----------------------------------------------------
 #WHILE
+
 n = 10                                 # --> 👉🏻 se declara la variable "n" (contador) y tiene asignado el valor 10.
 
 while n<10:                            # --> 👉🏻 se inicia un bucle "while" que tiene como condicion que se va a repetir
@@ -13,5 +15,18 @@ while n<10:                            # --> 👉🏻 se inicia un bucle "while"
         print(n,"es un numero impar")  # --> 👉🏻 se imprime "n, es un número impar".
     n+=1                               # --> 👉🏻 se indica que en cada iteración el contador (variable n) incrementa en 1.
     
+# 🔸 Código Modificado 🔸
+
+n = 0                          # --> 👉🏻 se declara la variable "n" (contador) y tiene asignado el valor 0.
+
+while n < 6:                   # --> 👉🏻 se inicia un bucle "while" que tiene como condicion que se va a repetir
+                                         #mientras n sea menor que 6.
+    if (n == 2):               # --> 👉🏻 se declara una condición "si n es igual a 2"
+        print (n, "es 2")      # --> 👉🏻 se imprime "n, es 2".
+    else:                      # --> 👉🏻 si no se cumple la condición
+        print (n, "No es 2")   # --> 👉🏻 se imprime "n, no es 2".
+    n+=1                       # --> 👉🏻 se indica que en cada iteración el contador (variable n) incrementa en 1.
+
+
 #  IF ELSE ELIF
 # Construir un algoritmo con lo viste en clase bajo el mismo diagrama de flujo de la imagen que está en la carpeta assets del repo
