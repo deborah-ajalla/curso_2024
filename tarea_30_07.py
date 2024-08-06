907a8b6c0b07e2dc6e79a59bd643b75c5b10e996
deborah-ajalla <deborahajalla@gmail.com>
2024-08-06 16:41:44 -0300

    ejercicio set

---
+++ tarea_30_07.py
@@ -0,0 +1,26 @@
+# SETS
+# En una variable grupo:
+print ("\n ----------------------------------------------")
+grupo = {"Fernando", "Federico", "Francisco", "Ricardo"}
+print ("Integrantes Iniciales del Grupo: \n", grupo)
+
+# 1) --> Añadir a las siguientes personas: Jose, Maria, Gerardo, Fabian
+
+grupo.add ("Jose") 
+grupo.add ("Maria")
+grupo.add ("Gerardo")
+grupo.add ("Fabian")
+print ("\n ----------------------------------------------")
+print ("Se añaden integrantes al grupo: \n", grupo)
+
+
+# 2) --> Eliminar a las personas: Fernando, Federico, Francisco
+
+
+
+#----------------------------------------------------------------------------------------------
+# DICCIONARIOS
+animalitos = {"elefante": ""}
+# Añadir al diccionario las claves perro, gato y tucan con sus valores "Tobi", "Michi" y "Diego"
+
+# Modificar la clave elefante por delfin
