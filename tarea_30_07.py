5e6cd85cebb812e7634560c1f79a4ee08a4cb1e0
deborah-ajalla <deborahajalla@gmail.com>
2024-08-06 16:55:08 -0300

    eliminacion en set

--- tarea_30_07.py
+++ tarea_30_07.py
@@ -15,11 +15,20 @@ print ("Se añaden integrantes al grupo: \n", grupo)
 
 
 # 2) --> Eliminar a las personas: Fernando, Federico, Francisco
+print ("\n ----------------------------------------------")
+grupo.remove("Fernando")
+print ("Grupo actualizado sin 'Fernando': \n", grupo)
+print ("\n")
 
+grupo.remove ("Federico")
+print ("Grupo actualizado sin 'Federico': \n", grupo)
+print ("\n")
 
-
+grupo.remove ("Francisco")
+print ("Grupo actualizado sin 'Francisco': \n", grupo)
 #----------------------------------------------------------------------------------------------
 # DICCIONARIOS
+print ("\n ----------------------------------------------")
 animalitos = {"elefante": ""}
 # Añadir al diccionario las claves perro, gato y tucan con sus valores "Tobi", "Michi" y "Diego"
 
